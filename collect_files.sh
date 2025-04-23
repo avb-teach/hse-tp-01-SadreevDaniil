a2137d31e75f0ed231071d3ba2c15c25a5aa3ceb
root <root@vm23557.de.snk.wtf>
2025-04-23 18:50:49 +0200

    прога 1

---
+++ collect_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+input_dir="$1"
+output_dir="$2"
+mkdir -p "$output_dir"
+find "$input_dir" -type f | while read -r file; do
+  filename=$(basename "$file")
+  cp "$file" "$output_dir/$filename"
+done

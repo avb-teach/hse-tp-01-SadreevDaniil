2b010a30fdbe7efb98286fdf82f6ea1a60493517
root <root@vm23557.de.snk.wtf>
2025-04-26 11:40:12 +0200

    финальная верся

--- collect_files.sh
+++ collect_files.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-input_dir="$1"
-output_dir="$2"
+if [[ $1 == "--max_depth" ]]; then
+  max_depth="$2"        
+  input_dir="$3"        
+  output_dir="$4"        
+  val=( -maxdepth "$max_depth" )
+else 
+  input_dir="$1"         
+  output_dir="$2"        
+  val=() 
+fi
+
 mkdir -p "$output_dir"
-find "$input_dir" -type f | while read -r file; do
-  filename=$(basename "$file")
-  cp "$file" "$output_dir/$filename"
+
+declare -A arr
+
+find "$input_dir" "${val[@]}" -type f | while IFS= read -r file; do
+
+  base=$(basename "$file")       
+  if [[ -v arr[$base] ]]; then   
+    arr[$base]=$(( arr[$base] + 1 ))
+    ext=${base##*.}               
+    name=${base%.*}              
+    base="${name}_${arr[$base]}.$ext"  
+  else
+    arr[$base]=0                
+  fi
+
+  cp "$file" "$output_dir/$base"
+  
 done
+
